fdbac12439f788c5e614c6d38f2ea329082189a4
AlexRico10 <alexrico_dev@hotmail.com>
2026-09-10 12:49:19 +0200

    mejoras

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -5,8 +5,16 @@ import Section from "../ui/Section/Section";
 import SectionIndicator from "../ui/SectionIndicator/SectionIndicator";
 import { useTranslations } from "use-intl";
 
+
+
 const Stack = () => {
   const t = useTranslations("Stack");
+
+  const stackBackend = ["C# · .NET", "Node.js · NestJS", "Microsoft SQL Server", "PostgreSQL", "Prisma"]
+  const stackFrontend = ["React · Next.js", "JavaScript ES6+", "TypeScript", "HTML5", "CSS3"]
+  const stackNoCode = ["WordPress", "Elementor · Divi", "SEO", "Zoho Forms", "Catalyst"]
+  const stackTools = ["VS Code", "Postman", "Jira", "Github"]
+
   return (
     <Section id="stack">
       <div className={styles.stackContent}>
@@ -14,35 +22,27 @@ const Stack = () => {
         <div className={styles.stackContentText}>
           <div className={styles.stackGroup}>
             <p className={styles.stackGroupTitle}>{t("backendTitle")}</p>
-            <p className={styles.stackText}>C# · .NET</p>
-            <p className={styles.stackText}>Node.js</p>
-            <p className={styles.stackText}>Microsoft SQL Server</p>
-            <p className={styles.stackText}>PostgreSQL</p>
-            <p className={styles.stackText}>Prisma</p>
+            {stackBackend.map((item) => (
+              <p className={styles.stackText} key={item}>{item}</p>
+            ))}
           </div>
           <div className={styles.stackGroup}>
             <p className={styles.stackGroupTitle}>{t("frontendTitle")}</p>
-            <p className={styles.stackText}>React</p>
-            <p className={styles.stackText}>JavaScript ES6+</p>
-            <p className={styles.stackText}>TypeScript</p>
-            <p className={styles.stackText}>HTML5</p>
-            <p className={styles.stackText}>CSS3</p>
+            {stackFrontend.map((item) => (
+              <p className={styles.stackText} key={item}>{item}</p>
+            ))}
           </div>
           <div className={styles.stackGroup}>
             <p className={styles.stackGroupTitle}>{t("cmsTitle")}</p>
-            <p className={styles.stackText}>WordPress</p>
-            <p className={styles.stackText}>Elementor · Divi</p>
-            <p className={styles.stackText}>SEO</p>
-            <p className={styles.stackText}>Zoho Forms</p>
-            <p className={styles.stackText}>Catalyst</p>
+            {stackNoCode.map((item) => (
+              <p className={styles.stackText} key={item}>{item}</p>
+            ))}
           </div>
           <div className={styles.stackGroup}>
             <p className={styles.stackGroupTitle}>{t("toolsTitle")}</p>
-            <p className={styles.stackText}>VS Code</p>
-            <p className={styles.stackText}>Postman</p>
-            <p className={styles.stackText}>Jira</p>
-            <p className={styles.stackText}>Github</p>
-            <p className={styles.stackText}>Postman</p>
+            {stackTools.map((item) => (
+              <p className={styles.stackText} key={item}>{item}</p>
+            ))}
           </div>
         </div>
       </div>
